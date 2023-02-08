e9d69c0336609cf9a66ba2feeb2f16e26b4f1697
Taras Bunyk <tbunyk@gmail.com>
2023-02-08 17:31:00 +0100

    Draw flowing notes, improve performance by reducing buffer

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"image/color"
+	"time"
 
 	"github.com/bunyk/fasolasi/ear"
 	"github.com/bunyk/fasolasi/notes"
@@ -28,45 +29,101 @@ func run() {
 		panic(err)
 	}
 
+	// FPS tracking
+	frames := 0
+	second := time.Tick(time.Second)
+
 	// Audio input stuff
-	const bufLen = 11025
-	const sampleRate = 44100
-	visualizer := ear.NewVisualizer(bufLen, 44100)
+	const bufLen = 11025 / 2
+	const sampleRate = 44100 / 2
+	visualizer := ear.NewVisualizer(bufLen, sampleRate)
 	detector := yin.NewYin(sampleRate, bufLen, 0.05)
 
+	start := time.Now()
+	played := make([]playedNote, 0, 100)
 	for !win.Closed() {
-
+		t := time.Since(start).Seconds()
 		buf := visualizer.Buffer()
 		pitch := detector.GetPitch(buf)
 		detector.Clean()
 		note, nn := notes.GuessNote(pitch)
 		if nn > 0 {
-			fmt.Println(pitch, note, nn)
+			if len(played) == 0 || played[len(played)-1].end > 0 { // no note currently playing
+				played = append(played, playedNote{ // create new note
+					start: t,
+					line:  note.Line,
+				})
+			} else if played[len(played)-1].line != note.Line { // note changed
+				played[len(played)-1].end = t       // end current one
+				played = append(played, playedNote{ // create new note
+					start: t,
+					line:  note.Line,
+				})
+			}
+		} else { // no note
+			if len(played) > 0 && played[len(played)-1].end == 0 { // there is a note
+				played[len(played)-1].end = t // end it
+			}
 		}
 
 		win.Clear(colornames.Whitesmoke)
 
 		lineGraph(win, colornames.Blue, buf)
-		drawNote(win, note.Line, note.Name)
+		drawNotes(win, note.Line, note.Name, played, t)
 		win.Update()
+
+		frames++
+		select {
+		case <-second:
+			win.SetTitle(fmt.Sprintf("FaSoLaSi | FPS: %d", frames))
+			frames = 0
+		default:
+		}
 	}
 }
 
-const noteRadius = 20
+type playedNote struct {
+	start float64
+	end   float64
+	line  int
+}
 
-func drawNote(t pixel.Target, line int, name string) {
+const noteRadius = 20
+const notePPS = 100 // pixels per second
+
+func time2X(time, currentTime float64) float64 {
+	return WIDTH/2 - (currentTime-time)*notePPS
+}
+
+func drawNotes(t pixel.Target, line int, name string, played []playedNote, time float64) {
 	imd := imdraw.New(nil)
 	imd.Color = colornames.Black
-	y := HEIGHT/2 - noteRadius*4
+	y := float64(HEIGHT/2 - noteRadius*4)
 	for i := 0; i <= 8; i += 2 {
 		imd.Push(
-			pixel.V(0, float64(y+i*noteRadius)),
-			pixel.V(WIDTH, float64(y+i*noteRadius)),
+			pixel.V(0, y+float64(i)*noteRadius),
+			pixel.V(WIDTH, y+float64(i)*noteRadius),
 		)
 		imd.Line(1)
 	}
+	imd.Color = colornames.Green
+	for _, note := range played {
+		if note.end > 0 && time2X(note.end, time) < 0 { // invisible already
+			continue
+		}
+		end := note.end
+		if end == 0 {
+			end = time
+		}
+		imd.Push(
+			pixel.V(time2X(note.start, time), y+(float64(note.line)-0.5)*noteRadius),
+			pixel.V(time2X(end, time), y+(float64(note.line)+0.5)*noteRadius),
+		)
+		imd.Rectangle(0)
+	}
 	if name != "pause" {
-		imd.Push(pixel.V(WIDTH/2, float64(y+line*noteRadius)))
+		imd.Color = colornames.Black
+		imd.Push(pixel.V(WIDTH/2, y+float64(line)*noteRadius))
 		imd.Circle(noteRadius, 0)
 	}
 	imd.Draw(t)
@@ -75,8 +132,8 @@ func drawNote(t pixel.Target, line int, name string) {
 func lineGraph(t pixel.Target, col color.Color, data []float64) {
 	imd := imdraw.New(nil)
 	imd.Color = col
-	for i := 0; i < len(data)/5; i++ {
-		imd.Push(pixel.V(float64(i)*0.5, HEIGHT*(0.5+data[i*5])))
+	for i := 0; i < WIDTH; i++ {
+		imd.Push(pixel.V(float64(i), HEIGHT*(0.1+data[i*len(data)/WIDTH])))
 	}
 	imd.Line(1)
 	imd.Draw(t)
