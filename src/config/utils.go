78c63ce1127b72516f3ce1dcec350df2467b6d96
Taras Bunyk <tbunyk@gmail.com>
2023-03-31 19:47:32 +0200

    Commit new files, downscale biggest sprite

---
+++ src/config/utils.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"fmt"
+	"image/color"
+
+	"golang.org/x/image/colornames"
+)
+
+func parseColor(s string) (c color.RGBA, err error) {
+	c, ok := colornames.Map[s]
+	if ok {
+		return c, nil
+	}
+	c.A = 0xff
+	switch len(s) {
+	case 7:
+		_, err = fmt.Sscanf(s, "#%02x%02x%02x", &c.R, &c.G, &c.B)
+	case 4:
+		_, err = fmt.Sscanf(s, "#%1x%1x%1x", &c.R, &c.G, &c.B)
+		// Double the hex digits:
+		c.R *= 17
+		c.G *= 17
+		c.B *= 17
+	default:
+		err = fmt.Errorf("invalid length, must be 7 or 4")
+
+	}
+	return
+}
